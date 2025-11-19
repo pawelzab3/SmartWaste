3a4d2904e5f0b9ddf1b92106bd16635ebb045573
pawelzab3 <pawelzab3@gmail.com>
2025-11-19 10:28:20 +0100

    poprawki

---
+++ frontend/src/app/components/components.module.ts
@@ -0,0 +1,13 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+imports: [
+  ReactiveFormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule
+]
