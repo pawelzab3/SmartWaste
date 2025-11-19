c7c44bf4dafa5a5321901e91ef37448fd87d40a2
pawelzab3 <pawelzab3@gmail.com>
2025-11-19 10:05:18 +0100

    konfiguracja bazy danych/ zainstalowanie paczek MySQL i ORM / utworzenie modułu zgłoszeń

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ReportsModule } from './reports/reports.module';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'smartwaste',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ReportsModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
